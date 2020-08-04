f2f919a043fd6db111fdeec68694e7de18b5eee6
Zac <zukaru@aol.com>
2020-08-04 13:42:51 -0500

    Added animation on feature items when scrolled in view

--- src/app/modules/landing-page/features/features.component.ts
+++ src/app/modules/landing-page/features/features.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, HostListener, Renderer2 } from '@angular/core';
+import { Subscription, fromEvent } from 'rxjs';
+import { throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-features',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./features.component.scss']
 })
 export class FeaturesComponent implements OnInit {
+  eventSubscription: Subscription
+  public innerWidth: number;
 
-  constructor() { }
+  @ViewChild('itemOne') elOne: ElementRef;
+  @ViewChild('itemTwo') elTwo: ElementRef;
+  @ViewChild('itemThree') elThree: ElementRef;
+
+  @HostListener('window:resize', ['$event'])
+    onResize() {
+    this.innerWidth = window.innerWidth;
+}
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    this.eventSubscription = fromEvent(document, "scroll")
+    .pipe(throttleTime(100))
+    .subscribe(e => {
+      this.animateOnScroll(this.elOne);
+      this.animateOnScroll(this.elTwo);
+      this.animateOnScroll(this.elThree);
+    })
   }
+
+  animateOnScroll(el: ElementRef) {
+    let introPosition = el.nativeElement.getBoundingClientRect().top;
+
+    if(this.innerWidth > 800) {
+      if(introPosition < this.innerWidth / 2) {
+        this.renderer.addClass(el.nativeElement, 'anim__fadeIn');
+      } else if(introPosition > this.innerWidth / 2) {
+        this.renderer.removeClass(el.nativeElement, 'anim__fadeIn')
+      }
+    } else if(introPosition < this.innerWidth + 250) {
+      this.renderer.addClass(el.nativeElement, 'anim__fadeIn');
+    } else if(introPosition > this.innerWidth + 250) {
+      this.renderer.removeClass(el.nativeElement, 'anim__fadeIn');
+    }
+
+  }
+
 
 }
